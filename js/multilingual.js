2ab3589c6363a48e919e32fccbe65217ed68f13a
graueneko <sczhg@sczhg.info>
2019-05-06 19:10:02 +0800

    Add in-page language switch for aklevel.html.

---
+++ js/multilingual.js
@@ -0,0 +1,23 @@
+﻿let langs = ["cn", "en"];
+
+function urlParam(sParam, defaultVal = undefined) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+    return defaultVal;
+}
+
+function getLang() {
+    return urlParam("lang", "cn");
+}
+
+let _lang = getLang();
+
+function showCurrentLang() {
+    $("." + _lang).show();
+};
