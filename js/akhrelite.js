92647149a69f4ee1dfbe44c37dffceae3b51f1b7
Aceship <terrenceleroy@yahoo.com>
2019-05-14 21:03:34 +0700

    Restyling elite calculator

--- js/akhrelite.js
+++ js/akhrelite.js
@@ -118,14 +118,19 @@
                     var name_cn = char.name_cn;
                     var name = eval('char.name_'+reg);
                     var nameTL = eval('char.name_'+lang);
-                    result.push({'name':name,'name_cn':name_cn,'nameTL':nameTL});
+                    var img_name = query(db.chars,"name",char.name_cn,true,true); 
+                    var rarity = img_name[Object.keys(img_name)].rarity + 1
+                    // console.log(rarity);
+                    result.push({'name':name,'name_cn':name_cn,'nameTL':nameTL,'img_name':Object.keys(img_name),rarity});
                 }
             });
             if(result.length > 0){
                 $('#operatorsResult').html("");
                 $('#operatorsResult').show();
                 for (var i = 0; i < result.length; i++) {
-                    $("#operatorsResult").append("<li style=\"cursor: pointer\" onclick=\"selectOperator('"+result[i].name_cn+"')\">"+result[i].nameTL+" ("+result[i].name+")"+"</li>");
+                    let image = `<img style="height:40px;padding:2px" src="./img/avatars/${result[i].img_name}_1.png">  `
+                    console.log(image)
+                    $("#operatorsResult").append("<li class=\"ak-shadow-small ak-mid ak-rare-"+result[i].rarity+"\"style=\"width:100%;cursor: pointer;margin-bottom:2px\" onclick=\"selectOperator('"+result[i].name_cn+"')\">"+image+result[i].nameTL+" ("+result[i].name+")"+"</li>");
                 }
             }
         } else {
@@ -147,6 +152,10 @@
         console.log(opdata2);
         $.each(opdata2,function(key,v){
             $("#opImage").attr('src','img/portraits/'+key+'_1.png');
+            // $("#opGlow").attr('src','img/ui/chara/glow-'+(opdata2[key].rarity+1)+'.png');
+            $("#opHeader").attr('src','img/ui/chara/header-'+(opdata2[key].rarity+1)+'.png');
+            $("#opBg").attr('src','img/ui/chara/bg-'+(opdata2[key].rarity<=2? 1:opdata2[key].rarity+1 )+'.png');
+            // $("#opBanner").attr('src','img/ui/chara/banner-'+(opdata2[key].rarity<=2? 1:opdata2[key].rarity+1 )+'.png');
             $("#opID").val(key);
             localStorage.selectedOP = key;
             return false
@@ -180,71 +189,75 @@
         console.log("SELECT ELITE");
         $("#tbody-materials").html("");
         $("#eliteDropBtn").html("Elite "+num);
-        reqmats = db.chars[$("#opID").val()].phases[num].evolveCost;
+        reqmats = db.chars[$("#opID").val()].phases[num] ? db.chars[$("#opID").val()].phases[num].evolveCost : undefined;
         var html = [];
-        $.each(reqmats,function(_,v){
-            var itemdata = db.items[v.id];
-            var itemdataTL = query(db.itemstl,"name_cn",itemdata.name);
-            html.push("<li>"
-                    +       "<div style=\"position: relative;\">"
-                    +           "<div class=\"item-name\">"+itemdataTL.name_en+"</div>"
-                    +           "<div class=\"item-image\">"
-                    +               "<span></span>"
-                    +               "<img id=\"item-image\" src=\"img/items/"+itemdata.iconId+".png\">"
-                    +           "</div>"
-                    +           "<img id=\"item-rarity\" src=\"img/material/bg/item-"+(itemdata.rarity+1)+".png\" width=\"150\">"
-                    +           "<div class=\"item-amount\">"+v.count+"x</div>"
-                    +       "</div>"
-                    +   "</li>");
+        if(reqmats){
+            
+            $.each(reqmats,function(_,v){
+                var itemdata = db.items[v.id];
+                var itemdataTL = query(db.itemstl,"name_cn",itemdata.name);
+                html.push("<li>"
+                        +       "<div style=\"position: relative;\">"
+                        +           "<div class=\"item-name\">"+itemdataTL.name_en+"</div>"
+                        +           "<div class=\"item-image\">"
+                        +               "<span></span>"
+                        +               "<img id=\"item-image\" src=\"img/items/"+itemdata.iconId+".png\">"
+                        +           "</div>"
+                        +           "<img id=\"item-rarity\" src=\"img/material/bg/item-"+(itemdata.rarity+1)+".png\" width=\"150\">"
+                        +           "<div class=\"item-amount\">"+v.count+"x</div>"
+                        +       "</div>"
+                        +   "</li>");
 
-            var tr = $("<tr></tr>");
-            var td = $("<td></td>");
-            var L1 = $("<div class=\"reqmats-container\"><li>"
-                    +       "<div style=\"position: relative;\">"
-                    +           "<div class=\"item-name\">"+itemdataTL.name_en+"</div>"
-                    +           "<div class=\"item-image\">"
-                    +               "<span></span>"
-                    +               "<img id=\"item-image\" src=\"img/items/"+itemdata.iconId+".png\">"
-                    +           "</div>"
-                    +           "<img id=\"item-rarity\" src=\"img/material/bg/item-"+(itemdata.rarity+1)+".png\" width=\"150\">"
-                    +           "<div class=\"item-amount\">"+v.count+"x</div>"
-                    +       "</div>"
-                    +   "</li></div>");
-            td.append(L1);
-            tr.append(td);
+                var tr = $("<tr></tr>");
+                var td = $("<td></td>");
+                var L1 = $("<div class=\"reqmats-container\"><li>"
+                        +       "<div style=\"position: relative;\">"
+                        +           "<div class=\"item-name\">"+itemdataTL.name_en+"</div>"
+                        +           "<div class=\"item-image\">"
+                        +               "<span></span>"
+                        +               "<img id=\"item-image\" src=\"img/items/"+itemdata.iconId+".png\">"
+                        +           "</div>"
+                        +           "<img id=\"item-rarity\" src=\"img/material/bg/item-"+(itemdata.rarity+1)+".png\" width=\"150\">"
+                        +           "<div class=\"item-amount\">"+v.count+"x</div>"
+                        +       "</div>"
+                        +   "</li></div>");
+                td.append(L1);
+                tr.append(td);
 
-            if(itemdata.buildingProductList.length>0){
-                var parentcount = v.count;
-                var formulaId = itemdata.buildingProductList[0].formulaId;
-                if(itemdata.buildingProductList[0].roomType == "MANUFACTURE"){
-                    var formula = db.manufactformulas[formulaId];
-                } else {
-                    var formula = db.workshopformulas[formulaId];
+                if(itemdata.buildingProductList.length>0){
+                    var parentcount = v.count;
+                    var formulaId = itemdata.buildingProductList[0].formulaId;
+                    if(itemdata.buildingProductList[0].roomType == "MANUFACTURE"){
+                        var formula = db.manufactformulas[formulaId];
+                    } else {
+                        var formula = db.workshopformulas[formulaId];
+                    }
+                    var td = $("<td style=\"vertical-align:middle;\"></td>");
+                    td.append("<div style=\"font-size:3em; font-weight: bold;\"><span><---</span></div>");
+                    tr.append(td);
+                    td = $("<td></td>");
+                    $.each(formula.costs,function(_,v){
+                        let itemdata = db.items[v.id];
+                        let itemdataTL = query(db.itemstl,"name_cn",itemdata.name);
+                        let li = $("<div class=\"reqmats-container\"><li>"
+                        +       "<div style=\"position: relative;\">"
+                        +           "<div class=\"item-name\">"+itemdataTL.name_en+"</div>"
+                        +           "<div class=\"item-image\">"
+                        +               "<span></span>"
+                        +               "<img id=\"item-image\" src=\"img/items/"+itemdata.iconId+".png\">"
+                        +           "</div>"
+                        +           "<img id=\"item-rarity\" src=\"img/material/bg/item-"+(itemdata.rarity+1)+".png\" width=\"150\">"
+                        +           "<div class=\"item-amount\">"+(v.count*parentcount)+"x</div>"
+                        +       "</div>"
+                        +   "</li></div>");
+                        td.append(li);
+                    });
+                    tr.append(td);
+                    $("#tbody-materials").append(tr);
                 }
-                var td = $("<td style=\"vertical-align:middle;\"></td>");
-                td.append("<div style=\"font-size:3em; font-weight: bold;\"><span><---</span></div>");
-                tr.append(td);
-                td = $("<td></td>");
-                $.each(formula.costs,function(_,v){
-                    let itemdata = db.items[v.id];
-                    let itemdataTL = query(db.itemstl,"name_cn",itemdata.name);
-                    let li = $("<div class=\"reqmats-container\"><li>"
-                    +       "<div style=\"position: relative;\">"
-                    +           "<div class=\"item-name\">"+itemdataTL.name_en+"</div>"
-                    +           "<div class=\"item-image\">"
-                    +               "<span></span>"
-                    +               "<img id=\"item-image\" src=\"img/items/"+itemdata.iconId+".png\">"
-                    +           "</div>"
-                    +           "<img id=\"item-rarity\" src=\"img/material/bg/item-"+(itemdata.rarity+1)+".png\" width=\"150\">"
-                    +           "<div class=\"item-amount\">"+(v.count*parentcount)+"x</div>"
-                    +       "</div>"
-                    +   "</li></div>");
-                    td.append(li);
-                });
-                tr.append(td);
-                $("#tbody-materials").append(tr);
-            }
-        });
+            });
+            
+        }
         $("#reqmats-container").html(html.join(""));
     }
 
