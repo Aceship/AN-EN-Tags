da8c25ded9753ebf10e2f2e23b1c0d24fbe0973f
Aceship <terrenceleroy@yahoo.com>
2019-06-26 09:40:54 +0700

    add linking to home on those arknight toolbox text

--- js/aknav.js
+++ js/aknav.js
@@ -113,7 +113,7 @@ $(document).ready(function(){
     }
     let html = `
     <img src="./img/factions/logo_rhodes.png" width="40" height="40" style="transform:scale(1.2,1.2)translate(-8px,1px)"class="d-inline-block align-top" alt="">
-        <a class="navbar-brand" href="#" translate-id="topbar-1">Arknights Toolbox</a>
+        <a class="navbar-brand" href="index.html" translate-id="topbar-1">Arknights Toolbox</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="展开">
             <span class="navbar-toggler-icon"></span>
