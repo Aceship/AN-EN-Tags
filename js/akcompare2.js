3a9cb1f8470ac23d27464b37a21fbd9f3cd549b7
Faryzal2020 <fary290796@gmail.com>
2020-02-02 20:27:20 +0900

    progress comparator mk2

--- js/akcompare2.js
+++ js/akcompare2.js
@@ -25,6 +25,8 @@ LoadAllJsonObjects(jsonList).then(function(result) {
 
 
 var slotnum;
+var opLevel = 1;
+var opElite = 0;
 
 $(document).ready(function(){
     //console.log(db.chars);
@@ -54,7 +56,7 @@ function RefreshSlots(){
     } catch (e) {
         localStorage.selectedOpList = JSON.stringify(selectedOpList);
     }
-    console.log(selectedOpList);
+    //console.log(selectedOpList);
 
     for (var i = 0; i < selectedOpList.length; i++) {
         var opID = selectedOpList[i];
@@ -62,21 +64,45 @@ function RefreshSlots(){
         var html = `
             <div class='opcontainer ak-shadow'>
                 <div class='removebtn float-right'>
-                    <a href='#' class='btn btn-info btn-sm' style='border-radius: 0px;' onclick='deleteOp(${i})'>
+                    <a href='#' class='btn btn-danger btn-sm' style='border-radius: 0px;' onclick='deleteOp(${i})'>
                     <i class='fa fa-close'></i></a></div>
                 <div class='opname ak-font-novecento lp-row'>${getENname(opData.name)}</div>
                 <div><img class='opimage ak-shadow mx-auto d-block' src='img/avatars/${opID}_1.png'></div>
 
 
-                <div id='stats-${opID}'>
+                <div id='slot-${i}-stats' style='margin-top:42px;'>
                     <div class='lp-row light'>
-                        test
+                        Elite <span id='slot-${i}-elite'>1</span>
+                    </div>
+                    <div class='lp-row light' style='margin-top:19px;'>
+                        Level <span id='slot-${i}-level'></span>
+                    </div>
+                    <div class='lp-row light' style='margin-top:23px;'>
+                        <span id='slot-${i}-maxHP'></span>
                     </div>
                     <div class='lp-row light'>
-                        test
+                        <span id='slot-${i}-atk'></span>
                     </div>
                     <div class='lp-row light'>
-                        test
+                        <span id='slot-${i}-def'></span>
+                    </div>
+                    <div class='lp-row light'>
+                        <span id='slot-${i}-mRes'></span>
+                    </div>
+                    <div class='lp-row light'>
+                        <span id='slot-${i}-rTime'></span> Seconds
+                    </div>
+                    <div class='lp-row light'>
+                        <span id='slot-${i}-cost'></span>
+                    </div>
+                    <div class='lp-row light'>
+                        <span id='slot-${i}-block'></span>
+                    </div>
+                    <div class='lp-row light'>
+                        <span id='slot-${i}-atkT'></span> Seconds
+                    </div>
+                    <div class='lp-row light'>
+                        <span id='slot-${i}-dps'></span>
                     </div>
                 </div>
             </div>
@@ -89,6 +115,103 @@ function RefreshSlots(){
                             +       "<i class='fa fa-plus'></i> "
                             +   "</button></div></div>");
     initDragScroll();
+    RefreshValues();
+}
+
+function RefreshValues(){
+
+    var selectedOpList = [];
+    try {
+        selectedOpList = JSON.parse(localStorage.selectedOpList);
+    } catch (e) {
+        localStorage.selectedOpList = JSON.stringify(selectedOpList);
+    }
+
+    for (var i = 0; i < selectedOpList.length; i++) {
+        var opID = selectedOpList[i];
+        var opData = eval('db.chars.'+opID);
+
+        var maxElite = opData.phases.length-1;
+        var elite = opElite;
+        if(maxElite < elite){ elite = maxElite}
+        var maxLevel = opData.phases[elite].attributesKeyFrames[1].level;
+        var level = opLevel;
+        if(maxLevel < level){ level = maxLevel}
+
+        $("#slot-"+i+"-elite").html(elite);
+        $("#slot-"+i+"-level").html(level);
+        $("#slot-"+i+"-maxHP").html(statsInterpolation('maxHp',level,elite,opData));
+        $("#slot-"+i+"-atk").html(statsInterpolation('atk',level,elite,opData));
+        $("#slot-"+i+"-def").html(statsInterpolation('def',level,elite,opData));
+        $("#slot-"+i+"-mRes").html(statsInterpolation('magicResistance',level,elite,opData));
+        $("#slot-"+i+"-rTime").html(statsInterpolation('respawnTime',level,elite,opData));
+        $("#slot-"+i+"-cost").html(statsInterpolation('cost',level,elite,opData));
+        $("#slot-"+i+"-block").html(statsInterpolation('blockCnt',level,elite,opData));
+        $("#slot-"+i+"-atkT").html(statsInterpolation('baseAttackTime',level,elite,opData));
+        var atkTime = statsInterpolation('baseAttackTime',level,elite,opData);
+        var atk = parseInt($("#slot-"+i+"-atk").html());
+        var dps = atk * (1/atkTime);
+        $("#slot-"+i+"-dps").html(parseInt(dps));
+    }
+    RefreshHighlight();
+}
+
+function RefreshHighlight(){
+    var selectedOpList = [];
+    try {
+        selectedOpList = JSON.parse(localStorage.selectedOpList);
+    } catch (e) {
+        localStorage.selectedOpList = JSON.stringify(selectedOpList);
+    }
+    var statNames = ['maxHP','atk','def','mRes','rTime','cost','block','atkT','dps'];
+    //console.clear();
+    for (var i = 0; i < statNames.length; i++) {
+        var maxVal = 0;
+        var minVal = 9999;
+        var slot = [];
+        for (var j = 0; j < selectedOpList.length; j++) {
+            $("#slot-"+j+"-"+statNames[i]).css({"font-weight":"unset","color":"white"});
+            var val = parseInt($("#slot-"+j+"-"+statNames[i]).html());
+
+            if(statNames[i] == 'rTime' || statNames[i] == 'cost' || statNames[i] == 'atkT'){
+                if(val < minVal){ 
+                    minVal = val;
+                    slot = [j]; 
+                } else if(val == minVal){
+                    slot.push(j);
+                }
+            } else {
+                if(val > maxVal){ 
+                    maxVal = val;
+                    slot = [j]; 
+                } else if(val == maxVal){
+                    slot.push(j);
+                }
+            }
+        }
+        if(slot.length > 1){
+            if(selectedOpList.length > slot.length){
+                for (var j = 0; j < 2; j++) {
+                    $("#slot-"+slot[j]+"-"+statNames[i]).css({"font-weight":"bold","color":"lightgreen"});
+                }
+            }
+        } else {
+            $("#slot-"+slot[0]+"-"+statNames[i]).css({"font-weight":"bold","color":"lightgreen"});
+        }
+    }
+}
+
+function changeLevel(val){
+    $("#levelSlider").val(val);
+    $("#levelDisplay").val(val);
+    opLevel = val;
+    RefreshValues();
+}
+
+function changeElite(val){
+    opElite = val;
+    changeLevel(1);
+    RefreshValues();
 }
 
 function selectOp(opID){
@@ -194,6 +317,25 @@ function selOpClass(cname){
             }
         });
         $("#selectedopclass").append(html);
+    }
+    
+}
+
+function statsInterpolation(key,level,elite_no,opdata,isround=true){
+    var kf = [];
+    $.each(opdata.phases[elite_no].attributesKeyFrames,function(j,v){
+        kf[j] = v;
+    });
+    //console.log([kf[0].level,kf[1].level])
+    //console.log([kf[0].data[key],kf[1].data[key]])
+    if(kf[0].data[key] == kf[1].data[key]){
+    return kf[0].data[key]
+    }else {
+        var pol = everpolate.linear([level],[kf[0].level,kf[1].level],[kf[0].data[key],kf[1].data[key]]);
+        if(isround)
+        return Math.round(pol);
+            else
+        return parseFloat(Math.round(pol*100))/100;
     }
     
 }
