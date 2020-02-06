a65a75392ba4ca3c8cb68edc1c0abd6c6d541ccb
Aceship <terrenceleroy@yahoo.com>
2020-02-06 16:58:14 +0700

    update data

---
+++ json/gamedata/kr/[uc]lua/hotfixes/definedfix.lua
@@ -0,0 +1,13 @@
+
+local list = 
+{
+  --list all defined fixer path at here
+  "HotFixes/ShopHotfixer",
+  "HotFixes/ShopFurnHotfixer",
+  "HotFixes/Act4d0Hotfixer",
+  "HotFixes/InventoryHotfixer",
+  "HotFixes/YostarSDKFixer",
+  "HotFixes/CheckinHotfixer"
+};
+
+return list;
