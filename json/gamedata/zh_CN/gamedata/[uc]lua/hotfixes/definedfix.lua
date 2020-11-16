0d8d64a9d0d29ea6ca46fb76b44b4358a49107ac
Aceship <terrenceleroy@yahoo.com>
2020-11-16 17:10:22 +0700

    update data + skin + medal

--- json/gamedata/zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ json/gamedata/zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -4,6 +4,10 @@ local list =
   --list all defined fixer path at here
   "HotFixes/ShopHotfixer",
   "HotFixes/SquadHotfixer",
+  "HotFixes/CampaignHotfixer",
+  "HotFixes/HomeHotfixer",
+  "HotFixes/BattleConverterHotFixer",
+  "HotFixes/BattleControllerHotfixer"
 };
 
 return list;
