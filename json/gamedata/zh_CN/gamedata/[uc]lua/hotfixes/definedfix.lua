dc9d499b21f310b5ca1442bfa9dc34222fcf878a
Aceship <terrenceleroy@yahoo.com>
2022-04-29 03:54:42 +0700

    update global data and recruit

--- json/gamedata/zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ json/gamedata/zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -4,8 +4,12 @@ local list =
   
   "HotFixes/ShopHotfixer",
   "HotFixes/DynamicMusicCompInfoHotfixer",
+  "HotFixes/ItemRepoHotfixer",
   "HotFixes/InParamsHotfixer",
-  "HotFixes/UIHotfixer"
+  "HotFixes/UIHotfixer",
+  "HotFixes/NameCardHotfixer",
+  "HotFixes/StagePreviewStateHotfixer",
+  "HotFixes/DeepSeaRPBattlePreviewStateHotfixer",
 };
 
 return list;
