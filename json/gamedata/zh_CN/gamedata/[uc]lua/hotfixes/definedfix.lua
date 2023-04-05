f2f82e6551f19bf5a169e97477519e758009085f
Aceship <terrenceleroy@yahoo.com>
2023-04-05 19:43:39 +0700

    update data

--- json/gamedata/zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
+++ json/gamedata/zh_CN/gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -3,6 +3,11 @@ local list =
 {
   
   "HotFixes/ShopHotfixer",
+  "HotFixes/HandbookHotfixer",
+  "HotFixes/TileHotfixer",
+  "HotFixes/MhwrctSkill_1Hotfixer",
+  "HotFixes/MhwrbgSkill_1Hotfixer",
+  "HotFixes/Yato2Skill_2Hotfixer"
 };
 
 return list;
